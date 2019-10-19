facd1e6579ac9d2780a1eb4d9a85c6592732c404
jkirtyan <jkirtyan@gmail.com>
2019-10-19 23:20:43 +0200

    Remove unnecessary rows

--- app/src/main/java/com/jkirtyan/exchangerate/MainActivity.kt
+++ app/src/main/java/com/jkirtyan/exchangerate/MainActivity.kt
@@ -2,21 +2,11 @@ package com.jkirtyan.exchangerate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val retrofit = Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create())
-            .baseUrl("https://revolut.duckdns.org/")
-            .build()
-
-        val service = retrofit.create<ExchangeRateService>(ExchangeRateService::class.java)
     }
 }
